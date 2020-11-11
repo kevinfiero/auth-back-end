725408a98caba90e9c1cfab5e3436c59f043f85b
kevinfiero <kjf46@cornell.edu>
2020-11-10 17:27:25 -0800

    added endpoints

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -10,6 +10,7 @@ async function run() {
     
     await client.query(`
             DROP TABLE IF EXISTS users CASCADE;
+            DROP TABLE IF EXISTS todo CASCADE;
             DROP TABLE IF EXISTS animals;
         `);
 
---
+++ data/todo.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'wash the dishes',
+    completed: false,
+},
+{
+  todo: 'feed dog',
+  completed: false,
+},
+{
+  todo: 'take shower',
+  completed: false,
+}
+];
